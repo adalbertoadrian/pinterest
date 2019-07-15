652563034f295bc91cdc82345cd67399708e26eb
Adalberto Adrian <adalberto.adrian@2080.cl>
2019-07-15 16:52:59 -0400

    registro de post en base de datos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,14 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.update(post_params)
+      flash[:success] = "Post guardado correctamente"
+      redirect_to posts_new_path(@post)
+    else
+      flash[:danger] = "Error al guardar Post"
+      redirect_to posts_new_path(@post)
+    end
   end
 
   def view
@@ -10,4 +18,10 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  private
+    # Valido los campos permitidos
+    def post_params
+      params.require(:post).permit(:title, :description, :img_id)
+    end
 end
